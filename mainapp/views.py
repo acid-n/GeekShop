22a4ee534b87b3bc53fcd0ef60e5bdc41d8b9b3b
Nikolay <acid_n@mail.ru>
2020-09-22 22:09:50 +0300

    Задания второго урока выполнены: - наследование шаблонов в проекте (подключили меню) - механизм подключения статики - шаблонные теги и фильтры - вывод динамического контента (элементы меню,заголовок страниц)

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
 
+links_menu = [
+    {'href': 'products_all', 'name': 'все'},
+    {'href': 'products_home', 'name': 'дом'},
+    {'href': 'products_office', 'name': 'офис'},
+    {'href': 'products_modern', 'name': 'модерн'},
+    {'href': 'products_classic', 'name': 'классика'},
+]
+
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    content = {
+        'title': 'главная страница',
+    }
+    return render(request, 'mainapp/index.html', content)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    content = {
+        'title': 'продукты',
+        'links_menu': links_menu,
+    }
+    return render(request, 'mainapp/products.html', content)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    content = {
+        'title': 'контакты',
+    }
+    return render(request, 'mainapp/contact.html', content)
