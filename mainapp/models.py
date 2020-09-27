f5a2e5083dd756ad23e9655af923303f1e3ec0f7
Nikolay <acid_n@mail.ru>
2020-09-27 21:32:38 +0300

    Задания третьего урока выполнены (update): - обновил немного на главной вывод изображения (проверил правильно ли настроил media). Работает.

--- mainapp/models.py
+++ mainapp/models.py
@@ -14,13 +14,17 @@ class ProductCategory(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, verbose_name='категория')
     name = models.CharField(max_length=128, verbose_name='имя')
-    image = models.ImageField(upload_to='products_images', blank=True)
+    image = models.ImageField(upload_to='products_images', blank=True, verbose_name='картинка')
     short_desc = models.CharField(max_length=128, blank=True, verbose_name='короткое описание')
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
-    quantity = models.PositiveSmallIntegerField(default=0)
+    description = models.TextField(blank=True, verbose_name='полное описание')
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0, verbose_name='цена')
+    quantity = models.PositiveSmallIntegerField(default=0, verbose_name='количество')
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
 
     def __str__(self):
         return f'{self.name} ({self.category.name})'
