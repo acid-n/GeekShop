424d52fe0fe536697d828c44d5548e256d5bb4a6
Nikolay <acid_n@mail.ru>
2020-09-30 21:17:34 +0300

    Задания четвертого урока (update): - разлогинится - редактирование профиля - валидация (еще надо поработать)

--- authapp/views.py
+++ authapp/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
 from django.urls import reverse
 
-from authapp.forms import ShopUserLoginForm
+from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm
 from django.contrib import auth
 from django.http import HttpResponseRedirect
 
 
 def login(request):
-    title = 'вход'
+    title = 'авторизация'
     login_form = ShopUserLoginForm(data=request.POST)
     content = {
         'title': title,
         'login_form': login_form
     }
 
+    # проверяю авторизован или нет, если авторизован, то на главную перекидываю, если нет то показываю форму
     if request.user.is_authenticated:
         return HttpResponseRedirect(reverse('main'))
     else:
@@ -31,3 +32,39 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('main'))
+
+
+def register(request):
+    title = 'регистрация'
+    if request.method == 'POST':
+        register_form = ShopUserRegisterForm(request.POST, request.FILES)
+
+        if register_form.is_valid():
+            register_form.save()
+            return HttpResponseRedirect(reverse('auth:login'))
+
+    else:
+        register_form = ShopUserRegisterForm()
+    content = {
+        'title': title,
+        'register_form': register_form
+    }
+    return render(request, 'authapp/register.html', content)
+
+
+def edit(request):
+    title = 'редактирование'
+    if request.method == 'POST':
+        edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('auth:edit'))
+    else:
+        edit_form = ShopUserEditForm(instance=request.user)
+
+    content = {
+        'title': title,
+        'edit_form': edit_form
+    }
+
+    return render(request, 'authapp/edit.html', content)
