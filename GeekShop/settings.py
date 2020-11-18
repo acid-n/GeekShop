ec0358f48eddf944d802c9fdcbd5766b03834972
Nikolay <acid_n@mail.ru>
2020-11-18 23:33:13 +0300

    Lesson 6 (update): fix static root

--- GeekShop/settings.py
+++ GeekShop/settings.py
@@ -161,12 +161,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-#     # '/var/www/static/',
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    # '/var/www/static/',
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
