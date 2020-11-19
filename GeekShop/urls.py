1361e796232a99c95908f4f67f1dffdfbf43787b
Nikolay <acid_n@mail.ru>
2020-11-19 14:09:32 +0300

    Lesson 6 (update): fix static root

--- GeekShop/urls.py
+++ GeekShop/urls.py
@@ -35,7 +35,6 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    import debug_toolbar
-
-    # urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
